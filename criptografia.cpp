0d1378a2a8b465b631b3a657aa25869f8a7fccf3
Paulo Lopes <58816081+pauloVato-sketch@users.noreply.github.com>
2021-11-22 20:46:03 -0300

    -Char aleatorio removido, função reoordenação de bloco criada

--- criptografia.cpp
+++ criptografia.cpp
@@ -9,18 +9,65 @@ Autor : Paulo Lopes do Nascimento
 Disciplina: Laboratório de Algoritmos e Estruturas de Dados
 Profª: Natalia Cosse Batista
 */
-
+/* Função para converter binario para decimal*/
+int converteBinarioDecimal(int binario)
+{
+    int valorDecimal = 0;
+    // Initializing base value to 1, i.e 2^0
+    int base = 1;
+ 
+    int temp = binario;
+    while (temp) {
+        int ultimoDigito = temp % 10;
+        temp = temp / 10;
+        valorDecimal += ultimoDigito * base;
+        base = base * 2;
+    }
+ 
+    return valorDecimal;
+}
 /*
     Função que separa, e remove o numero em binario e o caracter extra, retornando a string pura
 */
 std::string preencheEDecifra(std::string linha){ 
-    
+    int tamanho = linha.size();
+    std::string numeroBinario="";
+    int posicaoASerRemovida=0;
+    std::string stringPura="";
 
+    for(int i=0; i<tamanho; i++){
+        if(linha.at(i) >= 48 && linha.at(i) <= 57){
+            numeroBinario += linha.at(i);
+            if(numeroBinario.size() == 5){
+                posicaoASerRemovida = std::stoi( numeroBinario );
+                posicaoASerRemovida = converteBinarioDecimal(posicaoASerRemovida);
+            }
 
+        }else{
+            if( i != posicaoASerRemovida-1 + 5 ){
+                // std::cout << "POSICAO BINARIA - 1 = " << (posicaoASerRemovida - 1) << std::endl;
+                // std::cout << " i - POSICAO BINARIA - 1 = " << (i - posicaoASerRemovida-1) << std::endl;
+                stringPura += linha.at(i);
+            }
+        }
+
+    }
+    return stringPura;
 }
+/*
+    Função que reordena os blocos e inverte a reflexão
+*/
+std::string reoordenarBlocos(std::string alterada){
+    
+}
+/* 
+    Função que lê o arquivo e faz chamadas subsequentes para decriptografia
+*/
 void leArquivo(std::string nomeArquivo){
 
+    //String alterada
     std::string alteredCarbon;
+    //String linha
     std::string linha;
 
     std::ifstream arquivo(nomeArquivo);
@@ -28,12 +75,12 @@ void leArquivo(std::string nomeArquivo){
 
         while(std::getline(arquivo,linha)){
             alteredCarbon = preencheEDecifra(linha);
-            
-            
-        
+            alteredCarbon = reoordenarBlocos(alteredCarbon);
         }
         
-	}
+	}else{
+        std::cout << "Não foi possível abrir o arquivo. Tente novamente." << std::endl;
+    }
 }
 
 int main(){
@@ -42,7 +89,7 @@ int main(){
 
     std::cout << "Digite o nome do arquivo: ";
     std::cin  >> nomeArquivo; std::cout << std::endl;
-
+    leArquivo(nomeArquivo);
 
 
 }
