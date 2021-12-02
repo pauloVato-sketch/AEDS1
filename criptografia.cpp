bd696a0d3796d20c37d8eb3704bbc46daa7ecbdc
Paulo Lopes <58816081+pauloVato-sketch@users.noreply.github.com>
2021-12-02 14:27:29 -0300

    "Finished criptografia.cpp"

--- criptografia.cpp
+++ criptografia.cpp
@@ -2,7 +2,8 @@
 #include<fstream>
 #include<vector>
 #include<string>
-#include <ctype.h>
+#include<ctype.h>
+#include <algorithm>
 /**
 Criptografia
 Autor : Paulo Lopes do Nascimento
@@ -34,10 +35,10 @@ std::string preencheEDecifra(std::string linha){
     int posicaoASerRemovida=0;
     std::string stringPura="";
 
-    for(int i=0; i<linha.size(); i++){
+    for(int i=0; i< (int)linha.size(); i++){
         if(linha.at(i) >= 48 && linha.at(i) <= 57){
             numeroBinario += linha.at(i);
-            if(numeroBinario.size() == 5){
+            if((int)numeroBinario.size() == 5){
                 posicaoASerRemovida = std::stoi( numeroBinario );
                 posicaoASerRemovida = converteBinarioDecimal(posicaoASerRemovida);
             }
@@ -56,27 +57,81 @@ std::string preencheEDecifra(std::string linha){
 /*
     Função que reordena os blocos e inverte a reflexão
 */
-std::string reoordenarBlocos(std::string alterada){
+std::vector<std::string> reoordenarBlocos(std::string alterada){
     std::string stringTemp;
+    std::vector<std::string> vetorDeBlocos;
+    //std::cout << alterada << std::endl;
 
-    for(int i = 0; i < alterada.size(); i++){
-        if(i % 2 == 0){
-            alterada.insert(i,"-");
+    // Separa em blocos de 2
+    for(int i = 0; i < (int)alterada.size(); i++){
+        if( (int)stringTemp.size() == 2 ){
+            vetorDeBlocos.push_back(stringTemp);
+            stringTemp.clear();
+            stringTemp += alterada.at(i);
+        }else if(i == (int)alterada.size()-1){
+            stringTemp += alterada.at(i);
+            vetorDeBlocos.push_back(stringTemp);
+            stringTemp.clear();
+        }else{
+            stringTemp += alterada.at(i);
         }
-
     }
-    std::cout << alterada << std::endl;
     
-    return alterada;
+    // Inverte blocos
+    for(int i = 0; i < (int)vetorDeBlocos.size()/2; i++){
+        if( i % 2 == 0 ){
+            stringTemp = vetorDeBlocos.at(i);
+            vetorDeBlocos.at(i) = vetorDeBlocos.at((int)vetorDeBlocos.size() - i - 1);
+            vetorDeBlocos.at((int)vetorDeBlocos.size() - i - 1) = stringTemp;
+        }
+    }
+    // Reflete os blocos
+    for(int i = 0; i < (int)vetorDeBlocos.size(); i++){
+        std::reverse(vetorDeBlocos.at(i).begin(), vetorDeBlocos.at(i).end());
+    }
+    // for (std::string i: vetorDeBlocos)
+    //     std::cout << i << ' ';
+    //std::cout << std::endl;
+    return vetorDeBlocos;
+}
+/*
+    Função cifraDeCesar
+*/
+std::string cifraDeCesar(std::vector<std::string> vetorBlocos){
+    std::string retornoFinal;
+    for(int i = 0; i < (int)vetorBlocos.size(); i++){
+        for(int j = 0; j < (int)vetorBlocos.at(i).size(); j++){
+            if(vetorBlocos.at(i).at(j) == '#'){
+                retornoFinal += ' ';
+            }else{
 
+                if(vetorBlocos.at(i).at(j) >= 'a' && vetorBlocos.at(i).at(j) <= 'z'){
+                    char temp = vetorBlocos.at(i).at(j) - 5;
+                    if( temp < 'a'){
+                        temp = temp +'z' - 'a' + 1;
+                    }
+                    retornoFinal += temp;
+                }else if(vetorBlocos.at(i).at(j) >= 'A' && vetorBlocos.at(i).at(j) <= 'Z'){
+                        char temp = vetorBlocos.at(i).at(j) - 5;
+                        if( temp < 'A'){
+                            temp = temp +'Z' - 'A' + 1;
+                        }
+                        retornoFinal += temp;
+                }
+            }
+        }
+    }
+    return retornoFinal;
 }
 /* 
     Função que lê o arquivo e faz chamadas subsequentes para decriptografia
 */
-void leArquivo(std::string nomeArquivo){
+std::string leArquivo(std::string nomeArquivo){
 
     //String alterada
     std::string alteredCarbon;
+    //Vector de string com blocos refletidos
+    std::vector<std::string> vectorBlocos;
     //String linha
     std::string linha;
 
@@ -84,22 +139,31 @@ void leArquivo(std::string nomeArquivo){
     if(arquivo.is_open()){
 
         while(std::getline(arquivo,linha)){
+            std::cout << linha << std::endl << std::endl;
             alteredCarbon = preencheEDecifra(linha);
-            alteredCarbon = reoordenarBlocos(alteredCarbon);
+            vectorBlocos = reoordenarBlocos(alteredCarbon);
+            alteredCarbon = cifraDeCesar(vectorBlocos);
         }
         
 	}else{
         std::cout << "Não foi possível abrir o arquivo. Tente novamente." << std::endl;
     }
+    return alteredCarbon;
 }
 
 int main(){
 
-    std::string nomeArquivo;
+    std::string nomeArquivo, stringDecodificada;
 
     std::cout << "Digite o nome do arquivo: ";
     std::cin  >> nomeArquivo; std::cout << std::endl;
-    leArquivo(nomeArquivo);
-
+    std::cout << "--------------------"<< std::endl 
+              << "Mensagem codificada:"<< std::endl
+              << "--------------------"<< std::endl;
+    stringDecodificada = leArquivo(nomeArquivo);
+    std::cout << "----------------------"<< std::endl 
+              << "Mensagem decodificada:"<< std::endl
+              << "----------------------"<< std::endl;
+    std::cout << stringDecodificada << std::endl;
 
 }
