e05829855c811c2dbf5717eb0ebae51fa0cd95c5
Paulo Lopes <58816081+pauloVato-sketch@users.noreply.github.com>
2021-12-02 22:15:34 -0300

    criptografia.cpp comentada

--- criptografia.cpp
+++ criptografia.cpp
@@ -14,36 +14,51 @@ Profª: Natalia Cosse Batista
 int converteBinarioDecimal(int binario)
 {
     int valorDecimal = 0;
-    // Initializing base value to 1, i.e 2^0
+    // Inicializando valor base como 1 = 2^0
     int base = 1;
- 
+    
     int temp = binario;
+    //Enquanto houver digitos
     while (temp) {
+        //Divida por 10 e pegue o resto, que será o ultimo digito
         int ultimoDigito = temp % 10;
+        //Recebe o quociente da divisao acima
         temp = temp / 10;
+        //Soma o valor em decimal ao ultimo digito vezes a base
         valorDecimal += ultimoDigito * base;
+        //Altera a base
         base = base * 2;
     }
- 
+    //Retorna valor decimal
     return valorDecimal;
 }
 /*
     Função que separa, e remove o numero em binario e o caracter extra, retornando a string pura
 */
 std::string preencheEDecifra(std::string linha){ 
+    //String que receberá o numero binario
     std::string numeroBinario="";
+    //Posicao do caracter extra na linha
     int posicaoASerRemovida=0;
+    //StringPura é a string com numero e caracter extra removidos
     std::string stringPura="";
 
+    //Percorro a linha
     for(int i=0; i< (int)linha.size(); i++){
+        //Se o caracter da posição i for um numero...
         if(linha.at(i) >= 48 && linha.at(i) <= 57){
+            //... concateno para a string de numeroBinario
             numeroBinario += linha.at(i);
+            //Se a string de binario tiver 5 digitos, que é o limite
             if((int)numeroBinario.size() == 5){
+                //Converto a string binario para inteiro binario
                 posicaoASerRemovida = std::stoi( numeroBinario );
+                //Chamo a função que transforma binario em decimal
                 posicaoASerRemovida = converteBinarioDecimal(posicaoASerRemovida);
             }
-
         }else{
+            //Se o caracter da posição i não for um número e i diferente da posição que encontramos do 
+            //caracter falso, concatenamos o digito para a stringPura
             if( i != posicaoASerRemovida-1 + 5 ){
                 // std::cout << "POSICAO BINARIA - 1 = " << (posicaoASerRemovida - 1) << std::endl;
                 // std::cout << " i - POSICAO BINARIA - 1 = " << (i - posicaoASerRemovida-1) << std::endl;
@@ -52,75 +67,102 @@ std::string preencheEDecifra(std::string linha){
         }
 
     }
+    //Retorno a stringPura
     return stringPura;
 }
 /*
     Função que reordena os blocos e inverte a reflexão
 */
 std::vector<std::string> reoordenarBlocos(std::string alterada){
+    //String temporaria que será cada bloco e esvaziada quando chegar em n digitos
     std::string stringTemp;
+    //Criação de um vector de strings para separar os blocos por posição
     std::vector<std::string> vetorDeBlocos;
-    //std::cout << alterada << std::endl;
 
     // Separa em blocos de 2
     for(int i = 0; i < (int)alterada.size(); i++){
+        //Sea a string temporaria encher
         if( (int)stringTemp.size() == 2 ){
+            //Insiro ela no vector
             vetorDeBlocos.push_back(stringTemp);
+            //Limpo a string
             stringTemp.clear();
+            //Concateno o valor de agora
             stringTemp += alterada.at(i);
+            //Se for o ultimo i, pode ser o caso de faltar char no ultmo bloco, pra garantir
         }else if(i == (int)alterada.size()-1){
+            //Adiciona o ultimo char antes 
             stringTemp += alterada.at(i);
+            //Insiro no vector
             vetorDeBlocos.push_back(stringTemp);
+            //Limpo a string
             stringTemp.clear();
         }else{
+            //De resto, apenas adiciono o digito na string temporaria
             stringTemp += alterada.at(i);
         }
     }
     
-    // Inverte blocos
+    // Inverte blocos percorrendo ate a metade do vector
     for(int i = 0; i < (int)vetorDeBlocos.size()/2; i++){
+        //Se a posição for par
         if( i % 2 == 0 ){
+            //String temporaria recebe valor da posição i 
             stringTemp = vetorDeBlocos.at(i);
+            //Posição i é substituida pela posição inversa na ordem 
             vetorDeBlocos.at(i) = vetorDeBlocos.at((int)vetorDeBlocos.size() - i - 1);
+            //Posição inversa recebe o valor salvo na temporaria
             vetorDeBlocos.at((int)vetorDeBlocos.size() - i - 1) = stringTemp;
         }
     }
-    // Reflete os blocos
+    // Reflete os blocos usando std::reverse no vector
     for(int i = 0; i < (int)vetorDeBlocos.size(); i++){
         std::reverse(vetorDeBlocos.at(i).begin(), vetorDeBlocos.at(i).end());
     }
-    // for (std::string i: vetorDeBlocos)
-    //     std::cout << i << ' ';
-    //std::cout << std::endl;
+    //Retorna o vector de blocos
     return vetorDeBlocos;
 }
 /*
     Função cifraDeCesar
 */
 std::string cifraDeCesar(std::vector<std::string> vetorBlocos){
+    //Variavel que será o retorno final e definitivo da mensagem descriptografada
     std::string retornoFinal;
+    //Percorre o vector de strings
     for(int i = 0; i < (int)vetorBlocos.size(); i++){
+        //Percorre digito a digito das strings
         for(int j = 0; j < (int)vetorBlocos.at(i).size(); j++){
+            //Se o digito for #, concateno um espaço para a string final
             if(vetorBlocos.at(i).at(j) == '#'){
                 retornoFinal += ' ';
             }else{
-
+                //Se o digito estiver entre a e z minusculo
                 if(vetorBlocos.at(i).at(j) >= 'a' && vetorBlocos.at(i).at(j) <= 'z'){
+                    //Um char temporario recebe a posição rotacionada para a esquerda 5 posições
                     char temp = vetorBlocos.at(i).at(j) - 5;
+                    //Se a rotação fez com que o caracter saiu do limite
                     if( temp < 'a'){
+                        //Concertamos ao realizar a seguinte operação
                         temp = temp +'z' - 'a' + 1;
                     }
+                    //Concatena o temporario
                     retornoFinal += temp;
+                    //Se o digito estiver entre A e Z maiusculo
                 }else if(vetorBlocos.at(i).at(j) >= 'A' && vetorBlocos.at(i).at(j) <= 'Z'){
+                        //Um char temporario recebe a posição rotacionada para a esquerda 5 posições
                         char temp = vetorBlocos.at(i).at(j) - 5;
-                        if( temp < 'A'){
-                            temp = temp +'Z' - 'A' + 1;
-                        }
+                            //Se a rotação fez com que o caracter saiu do limite
+                            if( temp < 'A'){
+                                //Concertamos ao realizar a seguinte operação
+                                temp = temp +'Z' - 'A' + 1;
+                            }
+                        //Concatena o temporario
                         retornoFinal += temp;
                 }
             }
         }
     }
+    //Retorna a string com o retorno final
     return retornoFinal;
 }
 /* 
@@ -134,20 +176,25 @@ std::string leArquivo(std::string nomeArquivo){
     std::vector<std::string> vectorBlocos;
     //String linha
     std::string linha;
-
+    //Abre o arquivo
     std::ifstream arquivo(nomeArquivo);
     if(arquivo.is_open()){
-
+        // Percorre o arquivo pegando linha a linha
         while(std::getline(arquivo,linha)){
+            // Printa a linha para estar corretamente formatada
             std::cout << linha << std::endl << std::endl;
+            // String alterada recebe a linha depois de separar o binario e remover o caracter extra
             alteredCarbon = preencheEDecifra(linha);
+            // Vector de blocos recebe o retorno da função que separa a string alterada em blocos
             vectorBlocos = reoordenarBlocos(alteredCarbon);
+            // String alterada finalmente recebe o retorno da função que decifra a cifra de cesar
             alteredCarbon = cifraDeCesar(vectorBlocos);
         }
-        
+    // Se não conseguir acessar o arquivo exibe mensagem de erro (conveniente para programas completos)
 	}else{
         std::cout << "Não foi possível abrir o arquivo. Tente novamente." << std::endl;
     }
+    // Retorna a string definitiva
     return alteredCarbon;
 }
 
